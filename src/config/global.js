6d66c4fb3062a3d8bef135265b7b59e139e90daf
emilydev <emilse.a.b@gmail.com>
2024-05-28 17:20:44 -0500

    se vincula pdf accesible y descargable

--- src/config/global.js
+++ src/config/global.js
@@ -122,7 +122,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/31320003_CF2_DU.zip',
       },
       {
         icono: 'fas fa-download',
